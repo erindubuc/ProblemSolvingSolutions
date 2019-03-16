705bd9a70aca2b59960871b35760c0152ad85158
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-16 14:35:40 -0400

    Completed func to print a staircase of height n

---
+++ Staircase/Staircase/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the staircase function below.
+    static void staircase(int n)
+    {
+
+        for (int line = 0; line < n; line++)
+        {
+            // spaces on first line are = to n - 1
+            // then keep subtracting 1 space for each next line
+            for (int spaces = 1; spaces <= n - 1 - line; spaces++)
+                Console.Write(" ");
+
+            // hashes on first line
+            for (int hashes = 0; hashes < line + 1; hashes++)
+                Console.Write("#");
+
+            // start a new line
+            Console.WriteLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
