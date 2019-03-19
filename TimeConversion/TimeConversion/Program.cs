bc14017632dca9ee126f6e8c76e96e96e709f08b
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-19 13:01:08 -0400

    Completed func to convert 12 hour time format to military

---
+++ TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    /*Given a time in 12-hour AM/PM format, convert it to military 
+     * (24-hour) time.
+     * Complete the timeConversion function below.
+     */
+    static string timeConversion(string s)
+    {
+        DateTime military = new DateTime();
+        bool parsedTime = DateTime.TryParse(s, out military);
+        return military.ToString("HH:mm:ss");
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = timeConversion(s);
+
+        tw.WriteLine(result);
+
+        tw.Flush();
+        tw.Close();
+    }
+}
