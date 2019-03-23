c573840ce7c915acc351ac4dee240d69e28a5aba
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-22 20:24:02 -0400

    Refactored UberRide class and formatted output with currency format specifier

--- UberFareEstimator/UberFareEstimator/Program.cs
+++ UberFareEstimator/UberFareEstimator/Program.cs
@@ -6,59 +6,27 @@ using System.Threading.Tasks;
 
 namespace UberFareEstimator
 {
-    public class UberRide 
-    {
-        public int TimeLengthRide;
-        public int DistanceOfRide;
-        public double[] CostPerMinute;
-        public double[] CostPerMile;
-
-        public UberRide()
-        {
-
-        }
-
-        public UberRide(int ride_length, int ride_distance, double[] costPerMin, double[] costPerMile)
-        {
-            TimeLengthRide = ride_length;
-            DistanceOfRide = ride_distance;
-            costPerMin = CostPerMinute;
-            costPerMile = CostPerMile;
-       
-        }
-
-        public double[] FareEstimator(UberRide rideData)
-        {
-            
-            double[] totalFares = new double[CostPerMile.Length];
-            double cost;
-
-            for (int i = 0; i < CostPerMile.Length; i++)
-            {
-                cost = CostPerMinute[i] * TimeLengthRide + CostPerMile[i] * DistanceOfRide;
-                totalFares[i] = cost;
-            }
-
-            return totalFares;
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            
-            double[] costPerMin = { 0.2, 0.35, 0.4, 0.45 };
-            double[] costPerMile = { 1.1, 1.8, 2.3, 3.5 };
+            int TimeLengthRide = 30;
+            int DistanceOfRide = 7;
 
-            UberRide rideData = new UberRide(30, 7, costPerMin, costPerMile);
-            double[] result = rideData.FareEstimator(rideData);
+            double[] cost_per_minute = { 0.2, 0.35, 0.4, 0.45 };
+            double[] cost_per_mile = { 1.1, 1.8, 2.3, 3.5 };
+            double[] totalFares = new double[cost_per_mile.Length];
 
-            for (int i = 0; i < costPerMile.Length; i++)
+            for (int ride = 0; ride < cost_per_mile.Length; ride++)
             {
-                Console.WriteLine(result[i]);
-            }
+                UberRide vehicle = new UberRide(cost_per_minute[ride], cost_per_mile[ride]);
 
+                totalFares[ride] = vehicle.UberFareEstimator(TimeLengthRide, DistanceOfRide);
+
+                Console.WriteLine($"This Uber will cost: {totalFares[ride]:C}");
+                
+            }
+            
             Console.ReadLine();
         }
 
---
+++ UberFareEstimator/UberFareEstimator/UberRide.cs
@@ -0,0 +1,26 @@
+﻿namespace UberFareEstimator
+{
+    public class UberRide 
+    {
+        public int TimeLengthRide;
+        public int DistanceOfRide;
+        
+        double CostPerMinute;
+        double CostPerMile;
+
+        public UberRide(double costPerMin, double costPerMile)
+        {
+            this.CostPerMinute = costPerMin;
+            this.CostPerMile = costPerMile;
+        }
+
+        public double UberFareEstimator(int ride_time, int ride_distance)
+        {
+            TimeLengthRide = ride_time;
+            DistanceOfRide = ride_distance;
+            
+            return (ride_time) * (CostPerMinute) + (ride_distance) * (CostPerMile);
+        }
+
+    }
+}
