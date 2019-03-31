5ecdfcad5006c872418a0afcc5bd561850aee955
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-31 14:35:07 -0400

    Created func to return the century from given year

---
+++ yearToCentury.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+/* CodeSignal Arcade
+ * Given a year, return the century it is in. The first century spans from the 
+ * year 1 up to and including the year 100, the second - from the year 101 up 
+ * to and including the year 200, etc.
+ * 
+ */
+int centuryFromYear(int year)
+{
+
+    if (year <= 100)
+        return 1;
+
+    if (year >= 101 && year <= 200)
+        return 2;
+
+    if (year >= 201 && year <= 300)
+        return 3;
+
+    if (year >= 301 && year <= 400)
+        return 4;
+
+    if (year >= 401 && year <= 500)
+        return 5;
+
+    if (year >= 501 && year <= 600)
+        return 6;
+
+    if (year >= 601 && year <= 700)
+        return 7;
+
+    if (year >= 701 && year <= 800)
+        return 8;
+
+    if (year >= 801 && year <= 900)
+        return 9;
+
+    if (year >= 901 && year <= 1000)
+        return 10;
+
+    if (year >= 1001 && year <= 1100)
+        return 11;
+
+    if (year >= 1101 && year <= 1200)
+        return 12;
+
+    if (year >= 1201 && year <= 1300)
+        return 13;
+
+    if (year >= 1301 && year <= 1400)
+        return 14;
+
+    if (year >= 1401 && year <= 1500)
+        return 15;
+
+    if (year >= 1501 && year <= 1600)
+        return 16;
+
+    if (year >= 1601 && year <= 1700)
+        return 17;
+
+    if (year >= 1701 && year <= 1800)
+        return 18;
+
+    if (year >= 1801 && year <= 1900)
+        return 19;
+
+    if (year >= 1901 && year <= 2000)
+        return 20;
+
+    //if (year >=2001 && year <=2100)
+    return 21;
+}
