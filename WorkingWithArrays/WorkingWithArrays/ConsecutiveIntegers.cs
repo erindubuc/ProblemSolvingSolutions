7f6986989b9267f4d50eda89ae8f33d0da57833b
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-11 00:25:12 -0400

    Added method to make ints in array consecutive and return count of missing ints

---
+++ WorkingWithArrays/WorkingWithArrays/ConsecutiveIntegers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArrays
+{
+    class ConsecutiveIntegers
+    {
+        int MakeArrayConsecutive2(int[] statues)
+        {
+            List<int> newStatues = new List<int>();
+            Array.Sort(statues);
+
+            foreach (int statue in statues)
+            {
+                newStatues.Add(statue);
+                //Console.WriteLine(statue);
+            }
+
+            int listCount = newStatues.Count();
+            int firstNum = newStatues.First();
+            int lastNum = newStatues.Last();
+
+            int result = (lastNum - firstNum) + 1;
+
+            if (listCount != result)
+                return result - listCount;
+
+            return 0;
+        }
+
+    }
+}
