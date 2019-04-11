cee86cedda0382bcfd95274d765ed249cfd04d30
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-11 13:33:03 -0400

    Completed LINQ method to return whether an array has duplicate elements

---
+++ WorkingWithArrays/WorkingWithArrays/DuplicateElements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArrays
+{
+    class DuplicateElements
+    {
+        bool ContainsDuplicates(int[] a)
+        {
+            int aLength = a.Length;
+            IEnumerable<int> distinctNums = a.Distinct<int>();
+            int distinctLength = distinctNums.Count();
+
+            if (aLength > distinctLength)
+                return true;
+            return false;
+        }
+    }
+}
