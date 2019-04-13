8c9792b003b54666eb1ee786d8e25e08b0c6b0b8
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-12 21:34:48 -0400

    Completed method to determine whether two ints from 2 arrays provide a given sum

---
+++ WorkingWithArrays/WorkingWithArrays/SumOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArrays
+{
+    class SumOfTwo
+    {
+        bool SumOfTwoArrayIndexes(int[] a, int[] b, int v)
+        {
+
+            var hashSet = new HashSet<int>(a);
+
+            int temp = 0;
+
+            for (int bIndex = 0; bIndex < b.Length; bIndex++)
+            {
+                temp = v - b[bIndex];
+
+                if (hashSet.Contains(temp))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
