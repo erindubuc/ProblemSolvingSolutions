d438aed1234ee6e0519e40bf58f3d6c5a76814a6
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-14 10:58:33 -0400

    Completed method to return first duplicate value from an array if it exists using HashSet

---
+++ WorkingWithArrays/WorkingWithArrays/ReturnFirstDuplicate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArrays
+{
+    class ReturnFirstDuplicate
+    {
+        int FirstDuplicate(int[] a)
+        {
+            int duplicate = -1;
+
+            HashSet<int> possibleDup = new HashSet<int>();
+
+            foreach (int value in a)
+            {
+                if (possibleDup.Contains(value))
+                    return value;
+
+                possibleDup.Add(value);
+            }
+
+            return duplicate;
+        }
+    }
+}
