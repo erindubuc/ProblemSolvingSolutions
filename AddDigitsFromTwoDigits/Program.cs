a05243579597c03e6fd53264108df66614d167be
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-16 21:58:46 -0400

    Completed method to return sum of two digits from a two-digit number

---
+++ AddDigitsFromTwoDigits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddDigitsFromTwoDigits
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int result = addTwoDigits(number);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static int addTwoDigits(int n)
+        {
+            int firstDigit = n / 10;
+            int secondDigit = n % 10;
+
+            return firstDigit + secondDigit;
+        }
+
+    }
+
+}
