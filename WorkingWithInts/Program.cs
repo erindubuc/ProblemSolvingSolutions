1538ba58a192e60cc647ef3a897741f32a11298d
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-17 21:45:59 -0400

    Completed method to return the largest number containing n digits

---
+++ WorkingWithInts/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int result = LargestNumber(number);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static int LargestNumber(int n)
+        {
+            int largest = 9;
+
+            for (int digits = 1; digits < n; digits++)
+            {
+                largest = (largest * 10) + 9;
+            }
+            return largest;
+        }
+    }
+}
