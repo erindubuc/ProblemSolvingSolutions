b04fe81f23f44ab38b1c0f1c4936131757a3a16d
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-19 16:53:54 -0400

    Completed method to return total number of candies that can be eaten by n num of kids

---
+++ WorkingWithInts/HowManyCandies.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithInts
+{
+    public class HowManyCandies
+    {
+        public int GetNumberOfCandiesUsedInAll(int numOfKids, int piecesOfCandy)
+        {
+            int numOfCandies = 0;
+
+            if (numOfKids > piecesOfCandy)
+                numOfCandies = 0;
+
+            if (numOfKids < piecesOfCandy)
+                numOfCandies = (piecesOfCandy / numOfKids) * numOfKids;
+
+            if (numOfKids == piecesOfCandy)
+                numOfCandies = piecesOfCandy;
+
+            return numOfCandies;
+        }
+    }
+}
