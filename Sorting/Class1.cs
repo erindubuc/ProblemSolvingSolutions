a6e6eafbdaa13fe1c5cbe53c4a99e7cbb5fc22f7
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-24 22:20:48 -0400

    Completed method to sort an array with the bubble sort

---
+++ Sorting/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sorting
+{
+    public class BubbleSortMethod
+    {
+        void Swap(int[] items, int firstIndex, int secondIndex)
+        {
+            int temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+        }
+
+        int[] BubbleSort(int[] items)
+        {
+
+            int len = items.Length;
+
+            for (int indexOfItems = 0; indexOfItems < len; indexOfItems++)
+            {
+                for (int startIndex = 0, stop = len - startIndex; startIndex < stop - 1; startIndex++)
+                {
+                    if (items[startIndex] > items[startIndex + 1])
+                    {
+                        Swap(items, startIndex, startIndex + 1);
+                    }
+                }
+            }
+
+            return items;
+        }
+
+    }
+}
