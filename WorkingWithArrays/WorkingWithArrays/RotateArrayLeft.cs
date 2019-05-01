9d08540ecf51adb9049c9337d040f00dc64639ce
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-01 16:42:59 -0400

    Created method to rotate elements of an array to the left with a queue

---
+++ WorkingWithArrays/WorkingWithArrays/RotateArrayLeft.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArrays
+{
+    class RotateArrayLeft
+    {
+        static int[]RotateLeft(int[] arrayToShift, int numOfRotations)
+        {
+
+            Queue<int> arrayQ = new Queue<int>(arrayToShift.ToList());
+
+            while (numOfRotations-- > 0)
+            {
+                arrayQ.Enqueue(arrayQ.Dequeue());
+            }
+            return arrayQ.ToArray();
+        }
+    }
+}
