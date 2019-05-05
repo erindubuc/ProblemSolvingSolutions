f31a63af9a5ed2484c669eafb24e80b96b5c146b
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-05 14:51:24 -0400

    Completed method to return whether a ransom note could be replicated from a magazine

---
+++ HashTables_RansomNote/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System;
+
+class Solution
+{
+
+    // Complete the checkMagazine function below.
+    static void CheckMagazineForNoteWords(string[] magazine, string[] note)
+    {
+
+        Dictionary<string, int> magazineDictionary = new Dictionary<string, int>();
+        foreach (var word in magazine)
+        {
+            if (magazineDictionary.ContainsKey(word))
+                magazineDictionary[word]++;
+            else
+                magazineDictionary.Add(word, 1);
+        }
+
+        foreach (string word in note)
+        {
+
+            if (!magazineDictionary.ContainsKey(word) || magazineDictionary[word] < 1)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            else
+            {
+                magazineDictionary[word]--;
+            }
+
+        }
+        Console.WriteLine("Yes");
+    }
+
+    static void Main(string[] args)
+    {
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        string[] magazine = Console.ReadLine().Split(' ');
+
+        string[] note = Console.ReadLine().Split(' ');
+
+        CheckMagazineForNoteWords(magazine, note);
+    }
+}
+
