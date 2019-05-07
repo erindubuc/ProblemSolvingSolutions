e556437422c71e581c378d559e664ed6fcab211b
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-06 22:02:28 -0400

    Completed method to find the sum of a timer's digits

---
+++ LateRide/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateRide
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What does the timer say?");
+            int minutesPassed = int.Parse(Console.ReadLine());
+            int result = LateRideCurrentTime(minutesPassed);
+
+            Console.WriteLine($"Your ride took {minutesPassed} minutes and the sum of the timer's digits are {result}.");
+            Console.ReadLine();
+        }
+
+        public static int LateRideCurrentTime(int minutesPassed)
+        {
+            int currentTime = ((minutesPassed / 60) / 10) + ((minutesPassed / 60) % 10) + ((minutesPassed % 60) / 10) + ((minutesPassed % 60) % 10);
+
+            return currentTime;
+        }
+    }
+}
