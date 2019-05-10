55b8cacafdc44ea42005b714dea2b36b4ce4e2a8
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-09 22:45:21 -0400

    Method returns int for the total chars that must be deleted to make the strings anagrams

---
+++ Strings_MakingAnagrams/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_MakingAnagrams
+{
+    class Program
+    {
+
+        // Complete the makeAnagram function below.
+        static int MakeAnagram(string a, string b)
+        {
+            List<char> aChars = new List<char>();
+            List<char> bChars = new List<char>();
+
+            aChars.AddRange(a);
+            bChars.AddRange(b);
+
+            int totalChars = aChars.Count() + bChars.Count();
+
+            int charsInCommon = 0;
+
+            for (int charInA = 0; charInA < aChars.Count(); charInA++)
+            {
+                if (bChars.Contains(aChars[charInA]))
+                {
+                    charsInCommon++;
+                    bChars.Remove(aChars[charInA]);
+                }
+            }
+
+            return totalChars - (charsInCommon * 2);
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string a = Console.ReadLine();
+
+            string b = Console.ReadLine();
+
+            int res = MakeAnagram(a, b);
+
+            textWriter.WriteLine(res);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
