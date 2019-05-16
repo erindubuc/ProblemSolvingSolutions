eac62b733e02ad7ecc7d7c5e0a312e48a44231c6
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-16 16:27:26 -0400

    Method to determine the value of items from a treasure chest that you'd be able to carry

---
+++ WorkingWithInts/KnapSackWeightCapacity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithInts
+{
+    class KnapSackWeightCapacity
+    {
+        /*  You found two items in a treasure chest! The first item weighs weight1 
+         *  and is worth value1, and the second item weighs weight2 and is worth value2. 
+         *  What is the total maximum value of the items you can take with you, 
+         *  assuming that your max weight capacity is maxW and you can't come back for the items later?
+        */
+        int KnapsackLight(int value1, int weight1, int value2, int weight2, int maxW)
+        {
+            if (weight1 + weight2 <= maxW)
+                return value1 + value2;
+
+            if (weight1 + weight2 >= maxW)
+            {
+                if (weight1 <= maxW && weight2 <= maxW)
+                {
+                    if (value1 > value2)
+                        return value1;
+                    else
+                        return value2;
+                }
+
+                if (weight1 <= maxW)
+                {
+                    return value1;
+                }
+
+                if (weight2 <= maxW)
+                {
+                    return value2;
+                }
+            }
+
+            return 0;
+
+        }
+    }
+}
