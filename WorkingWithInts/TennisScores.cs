7647359c7b3533cb008e032b326a33af0fda1e63
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-19 11:43:19 -0400

    Method to determine if it's possible for a tennis set to be finished with score1:score2

---
+++ WorkingWithInts/TennisScores.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithInts
+{
+    class TennisScores
+    {
+        /*
+        Given two integers score1 and score2, your task is to 
+        determine if it is possible for a tennis set to be finished 
+            with a final score of score1 : score2.
+        */
+
+        bool WillTennisSetBeFinishedWithThisFinalScore(int score1, int score2)
+        {
+            if ((score1 == 6 && score2 < 5) || (score2 == 6 && score1 < 5))
+                return true;
+
+            if ((score1 == 7 && score2 >= 5 && score2 < score1) || (score2 == 7 && score1 >= 5 && score1 < score2))
+            {
+                if (score1 != score2)
+                    return true;
+                else
+                    return false;
+
+            }
+
+            return false;
+        }
+    }
+}
