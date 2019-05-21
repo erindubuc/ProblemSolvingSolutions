a935625bf5693489f8d3e7dab9f97c9c6cd09548
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-21 16:21:45 -0400

    Uses stack to find kth largest element in unsorted array

---
+++ InterviewChallenges/Stack_FindKthLargestElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewChallenges
+{
+    public class FindTheKthLargestElement
+    {
+        /*  Find the kth largest element in an unsorted array. 
+         *  This will be the kth largest element in sorted order, 
+         *  not the kth distinct element.
+        */
+
+        public int FindKthLargestElement(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            Stack<int> numsSorted = new Stack<int>(nums);
+
+            for (int numbersToPop = 0; numbersToPop < k - 1; numbersToPop++)
+            {
+                numsSorted.Pop();
+            }
+
+            return numsSorted.Pop();
+        }
+    }
+}
