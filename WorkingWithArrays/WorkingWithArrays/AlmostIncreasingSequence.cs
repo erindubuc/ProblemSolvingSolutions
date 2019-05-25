544f29aca742d6b318f34f24803f51a8b53e289f
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-25 16:40:06 -0400

    Returns bool whether nums in array are almost increasing by removing no more than one element

---
+++ WorkingWithArrays/WorkingWithArrays/AlmostIncreasingSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArrays
+{
+    class AlmostIncreasingSequence
+    {
+        /* Given a sequence of integers as an array, determine 
+             * whether it is possible to obtain a strictly increasing 
+             * sequence by removing no more than one element from the array.
+            */
+        bool IsThisAnAlmostIncreasingSequence(int[] sequence)
+        {
+            int numbersRemoved = 0;
+            int timesANumberWasRemoved = 0;
+
+            for (int index = 0; index < sequence.Length - 1; index++)
+            {
+                if (sequence[index] >= sequence[index + 1])
+                {
+                    numbersRemoved++;
+                    timesANumberWasRemoved++;
+
+                    if (timesANumberWasRemoved > 1)
+                        numbersRemoved++;
+                }
+                if (index + 2 < sequence.Length && sequence[index] >= sequence[index + 2])
+                    numbersRemoved++;
+
+            }
+            if (numbersRemoved <= 2)
+                return true;
+            return false;
+        }
+    }
+}
