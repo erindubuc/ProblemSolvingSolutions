b93a177815522b0f0bf853f817e4a331f5b80abb
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-06 23:09:50 -0400

    Returns first not repeating character from string

---
+++ WorkingWithStrings/FirstNotRepeatingCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class FirstNotRepeatingCharacter
+{
+    public char FindFirstNotRepeatingCharacterInString(string s)
+    {
+        foreach (char c in s)
+        {
+            if (s.LastIndexOf(c) == s.IndexOf(c))
+                return c;
+        }
+
+        return '_';
+    }
+}
