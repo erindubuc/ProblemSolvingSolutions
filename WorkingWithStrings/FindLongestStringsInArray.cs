223f443f1e04b5d30cbc93c1f7ab7c73253c1ebd
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-21 15:06:41 -0400

    Returns array of longest strings

---
+++ WorkingWithStrings/FindLongestStringsInArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class FindLongestString
+{
+    string[] FindAllLongestStringsInArray(string[] inputArray)
+    {
+        int lengthOfLongestString = FindLongestStringLength(inputArray);
+
+        List<string> longestStrings = new List<string>();
+
+        for (int word = 0; word < inputArray.Length; word++)
+        {
+            if (inputArray[word].Length == lengthOfLongestString)
+                longestStrings.Add(inputArray[word]);
+        }
+
+        return longestStrings.ToArray();
+    }
+
+    int FindLongestStringLength(string[] input)
+    {
+        int longestLengthOfString = 1;
+
+        for (int str = 0; str < input.Length; str++)
+        {
+            if (input[str].Length > longestLengthOfString)
+                longestLengthOfString = input[str].Length;
+        }
+
+        return longestLengthOfString;
+    }
+}
