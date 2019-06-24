e52e0bd03e6d22a6c1689eedc0630a6db43ac738
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-24 18:12:08 -0400

    Returns count of common chars between 2 strings

---
+++ WorkingWithStrings/CommonCharacterCount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class CommonCharacters
+{
+    int FindCommonCharacterCountOfTwoStrings(string s1, string s2)
+    {
+        Dictionary<char, int> charCounter = new Dictionary<char, int>();
+        int commonCharCount = 0;
+
+        foreach (char c in s1)
+        {
+            if (charCounter.ContainsKey(c))
+                charCounter[c]++;
+            else
+                charCounter.Add(c, 1);
+        }
+
+        foreach (char c in s2)
+        {
+            if (charCounter.ContainsKey(c) && charCounter[c] > 0)
+            {
+                charCounter[c]--;
+                commonCharCount++;
+
+            }
+        }
+
+        return commonCharCount;
+    }
+}
