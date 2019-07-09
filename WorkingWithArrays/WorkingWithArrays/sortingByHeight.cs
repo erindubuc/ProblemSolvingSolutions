b29fea83e4d1bd05086d0470118f5377b5fd8767
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-07-08 20:03:46 -0400

    Returns array of people by their heights without removing trees (-1)

---
+++ WorkingWithArrays/WorkingWithArrays/sortingByHeight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArrays
+{
+    class SortingByHeight
+    {
+        /*  Some people are standing in a row in a park. There are 
+         *  trees between them which cannot be moved. Your task is 
+         *  to rearrange the people by their heights in a non-descending 
+         *  order without moving the trees. People can be very tall!
+        */
+        int[] SortByHeight(int[] peopleAndTrees)
+        {
+            int[] sorted = new int[peopleAndTrees.Length];
+            Array.Copy(peopleAndTrees, sorted, peopleAndTrees.Length);
+            Array.Sort(sorted);
+
+            int sortIndex = sorted.Length - 1;
+
+            for (int index = peopleAndTrees.Length - 1; index >= 0; index--)
+            {
+                if (peopleAndTrees[index] != -1)
+                {
+                    peopleAndTrees[index] = sorted[sortIndex];
+                    sortIndex--;
+                }
+            }
+            return peopleAndTrees;
+        }
+    }
+}
